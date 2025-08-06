aac2a86a9169a1368aa4b8c10deb2767ae38cc33
刘志佳 <563086368@qq.com>
2025-08-06 11:40:49 +0800

    feat: 新增x6拓扑图编辑器页面

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -18,10 +18,16 @@ const MENU_CONFIG: MenuItem[] = [
     icon: 'graphy',
     children: [
       {
-        title: 'antv G6',
+        title: 'G6 树形图',
         path: '/graphy/g6',
         icon: 'antv',
         component: () => import('@/pages/graphy/g6/index.vue'),
+      },
+      {
+        title: 'X6 拓扑图编辑器',
+        path: '/graphy/x6',
+        icon: 'x6',
+        component: () => import('@/pages/graphy/x6/index.vue'),
       },
     ],
   },
---
+++ src/pages/graphy/x6/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>X6 拓扑图编辑器【待开发】</h1>
+    <div ref="container" class="graphy-x6-container"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const container = ref<HTMLDivElement>();
+</script>
